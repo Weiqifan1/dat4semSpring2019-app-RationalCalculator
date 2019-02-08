b1b5bbc909b3b84253ee533a6dbfe5907d772b35
Anders Kalhauge <anders@kalhauge.dk>
2019-02-08 09:17:16 +0100

    Initial

---
+++ app/src/main/java/dk/cphbusiness/rationalcalculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package dk.cphbusiness.rationalcalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
