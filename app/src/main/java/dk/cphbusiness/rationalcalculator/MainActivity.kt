fed74eeb9fe9a6f91efe16c8174ae2faf0aff4cd
Anders Kalhauge <anders@kalhauge.dk>
2019-02-11 10:36:00 +0100

    first buttons

--- app/src/main/java/dk/cphbusiness/rationalcalculator/MainActivity.kt
+++ app/src/main/java/dk/cphbusiness/rationalcalculator/MainActivity.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.calculator_buttons.*
+import kotlinx.android.synthetic.main.calculator_keypad.*
 import kotlinx.android.synthetic.main.calculator_stack.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var calculator: Calculator
+    var inputValue: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +23,10 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
         // val eb = findViewById<Button>(R.id.enterButton)
+/*
         enterButton.setOnClickListener {
             val number = editText2.text.toString().toLongOrNull()
+
             if (number == null) {
                 val top = calculator.stack1
                 calculator.push(top)
@@ -31,6 +35,7 @@ class MainActivity : AppCompatActivity() {
             updateStack()
             editText2.setText("")
             }
+*/
         updateStack()
         }
 
@@ -44,10 +49,10 @@ class MainActivity : AppCompatActivity() {
       }
 
     fun checkInput() {
-        val number = editText2.text.toString().toLongOrNull()
+        val number = inputField.text.toString().toLongOrNull()
         if (number != null) {
             calculator.enter(number)
-            editText2.setText("")
+            inputField.setText("")
             }
         }
 
@@ -56,16 +61,36 @@ class MainActivity : AppCompatActivity() {
         stack3.setText(calculator.stack3.text)
         stack2.setText(calculator.stack2.text)
         stack1.setText(calculator.stack1.text)
+        inputField.setText(inputValue.toString())
         }
 
     fun doOperation(view: View) {
-        checkInput()
+        fun updateInput(digit: Int) { inputValue = 10*inputValue + digit}
+        // checkInput()
         when (view) {
-            minusButton -> calculator.minus()
-            plusButton -> calculator.plus()
-            timesButton -> calculator.times()
-            divButton -> calculator.div()
-            // divButton -> calculator = calculator.nyDiv()
+            button0 -> updateInput(0)
+            button1 -> updateInput(1)
+            button2 -> updateInput(2)
+            button3 -> updateInput(3)
+            button4 -> updateInput(4)
+            button5 -> updateInput(5)
+            button6 -> updateInput(6)
+            button7 -> updateInput(7)
+            button8 -> updateInput(8)
+            button9 -> updateInput(9)
+            buttonEnter -> {
+                if (inputValue != 0L) calculator.enter(inputValue)
+                else {
+                    val top = calculator.stack1
+                    calculator.push(top)
+                    }
+                inputValue = 0L
+                }
+            buttonPlus -> {
+                if (inputValue != 0L) calculator.enter(inputValue)
+                calculator.plus()
+                inputValue = 0L
+                }
             }
         updateStack()
         }
